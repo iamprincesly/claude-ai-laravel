48cb577fb148d03b34547d70ec0509ac5f042753
Sylvanus Etim <64446276+iamprincesly@users.noreply.github.com>
2024-11-01 03:43:13 +0100

    Update claude-laravel.php

--- config/claude-laravel.php
+++ config/claude-laravel.php
@@ -7,9 +7,6 @@ return [
     | CLAUDE AI API KEY
     |--------------------------------------------------------------------------
     |
-    | Here you may specify which of the locking mechanism to us when altering
-    | wallet balance to avoid race condition
-    |
     */
     'api_key' => env('CLAUDE_API_KEY'),
 
@@ -17,9 +14,6 @@ return [
     |--------------------------------------------------------------------------
     | CLAUDE AI API VERSION
     |--------------------------------------------------------------------------
-    |
-    | Here you may specify which of the locking mechanism to us when altering
-    | wallet balance to avoid race condition
     |
     */
     'api_version' => env('CLAUDE_API_VERSION', '2023-06-01'),
@@ -29,9 +23,6 @@ return [
     | CLAUDE AI MODEL
     |--------------------------------------------------------------------------
     |
-    | Here you may specify which of the locking mechanism to us when altering
-    | wallet balance to avoid race condition
-    |
     */
     'model' => env('CLAUDE_MODEL', 'claude-3-5-sonnet-20241022'),
 
@@ -39,9 +30,6 @@ return [
     |--------------------------------------------------------------------------
     | CLAUDE AI MAX TOKENS
     |--------------------------------------------------------------------------
-    |
-    | Here you may specify which of the locking mechanism to us when altering
-    | wallet balance to avoid race condition
     |
     */
     'max_tokens' => env('CLAUDE_MAX_TOKENS', 1024),
